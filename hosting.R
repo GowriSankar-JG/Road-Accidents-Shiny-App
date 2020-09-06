3b82dab1529f57dc82ed696afc76a35f256d39db
Gowrisankar JG <43264841+GowriSankar-JG@users.noreply.github.com>
2020-09-06 22:33:45 +0530

    Add files via upload

---
+++ hosting.R
@@ -0,0 +1,5 @@
+# install.packages('rsconnect')
+rsconnect::setAccountInfo(name='gowrisankarjg', 
+                          token='DD81392F3ED9F6DAEB8276B7F4D7B0BE', 
+                          secret='WG/s6pNnxvBTU2IIuwF3YvW9Ufme53ojIlrnuasq')
+rsconnect::deployApp()
